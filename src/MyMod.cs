4f9965a0c0b0ae40f7ee5cc18ca7b80e4599dab0
camdendees31-jpg <camdendees31@gmail.com>
2026-04-21 23:48:41 -0500

    Update MyMod.cs

--- src/MyMod.cs
+++ src/MyMod.cs
@@ -1,1 +1,18 @@
+using UnityEngine;
 
+namespace MyGorillaTagMod
+{
+    public class MyMod : MonoBehaviour
+    {
+        void Update()
+        {
+            // Your mod logic here
+            // Example: speed boost
+        }
+
+        void OnGUI()
+        {
+            GUI.Label(new Rect(10, 10, 300, 30), "MyGorillaTagMod is running!");
+        }
+    }
+}
