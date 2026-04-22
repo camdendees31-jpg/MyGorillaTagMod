c2936e4b94c87410e4f56afdcc3eff3a09f3c99f
camdendees31-jpg <camdendees31@gmail.com>
2026-04-21 23:48:10 -0500

    Update Plugin.cs

--- src/Plugin.cs
+++ src/Plugin.cs
@@ -1,1 +1,18 @@
+using BepInEx;
+using UnityEngine;
 
+namespace MyGorillaTagMod
+{
+    [BepInPlugin("com.you.mygorillatagmod", "MyGorillaTagMod", "1.0.0")]
+    public class Plugin : BaseUnityPlugin
+    {
+        void Awake()
+        {
+            Logger.LogInfo("MyGorillaTagMod loaded!");
+            // Add your GameObject with mod component
+            var go = new GameObject("MyModObject");
+            go.AddComponent<MyMod>();
+            DontDestroyOnLoad(go);
+        }
+    }
+}
